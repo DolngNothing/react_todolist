45cd5286ddadc02804f137d057498f4be57822b7
DolngNothing <1178363215@qq.com>
2020-08-04 21:00:50 +0800

    [TODOLIST][Shao, Christopher] feature: add axios get

--- src/components/TodoList/index.jsx
+++ src/components/TodoList/index.jsx
@@ -1,7 +1,14 @@
 import TodoContainer from "../../containers/TodoContainer";
 import React from "react";
+import axios from 'axios'
 
 export default class TodoList extends React.Component {
+  componentDidMount(){
+    axios.get("https://5e9ec500fb467500166c4658.mockapi.io/todos").then((response)=>{
+      console.log(response);
+    })
+  }
+
   render() {
     return this.props.todoList.map((todo, index) => (
       <div>
